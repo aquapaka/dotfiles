931b80640eac7d58a6ba6d18734ffc0d6f6b598f
Tam Long <aqua.tamlong@gmail.com>
2023-03-28 16:55:52 +0700

    add .local scripts and asciiart

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -7,3 +7,7 @@ cp -rs --remove-destination $SCRIPTPATH/../.config/bspwm ~/.config
 cp -rs --remove-destination $SCRIPTPATH/../.config/alacritty ~/.config
 cp -rs --remove-destination $SCRIPTPATH/../.config/zsh ~/.config
 cp -rs --remove-destination $SCRIPTPATH/../.config/gtk-3.0 ~/.config
+ln -fs $SCRIPTPATH/../home/.zshrc ~/.zshrc
+ln -fs $SCRIPTPATH/../home/.xinitrc ~/.xinitrc
+ln -fs $SCRIPTPATH/../home/.fehbg ~/.fehbg
+cp -rs --remove-destination $SCRIPTPATH/../.local ~
