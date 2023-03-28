464aeb544eec14e5570e31671fe52ca6fe85f6b7
Tam Long <aqua.tamlong@gmail.com>
2023-03-28 13:30:54 +0700

    Init all files

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -2,4 +2,8 @@
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-ln -sf $SCRIPTPATH/../.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+cp -rs --remove-destination $SCRIPTPATH/../.config/sxhkd ~/.config
+cp -rs --remove-destination $SCRIPTPATH/../.config/bspwm ~/.config
+cp -rs --remove-destination $SCRIPTPATH/../.config/alacritty ~/.config
+cp -rs --remove-destination $SCRIPTPATH/../.config/zsh ~/.config
+cp -rs --remove-destination $SCRIPTPATH/../.config/gtk-3.0 ~/.config
