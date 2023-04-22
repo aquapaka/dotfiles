945f4d919520f25ccafa8b116331909b534f981e
Tam Long <aqua.tamlong@gmail.com>
2023-04-22 13:43:29 +0700

    Add script to remove everything

--- scripts/remove-everything.sh
+++ scripts/remove-everything.sh
@@ -3,5 +3,5 @@
 echo "Start remove all but essential packages";
 sudo pacman -D --asdeps $(pacman -Qqe);
 sudo pacman -D --asexplicit base linux linux-firmware sudo vim networkmanager;
-sudo pacman -Qtdq | pacman -Rns -
+sudo pacman -Qtdq | sudo pacman -Rns -
 
