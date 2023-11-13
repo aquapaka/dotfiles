c126d7fd6306177b6fb483779f8f5bfdd94ad078
aquapaka <aqua.tamlong@gmail.com>
2023-11-13 15:54:33 +0700

    Add powershell autocomplete menu & history autocomplete

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+Invoke-Expression (&starship init powershell)
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
