ca5944a356204b6c5a36b273da3e9e41622bba64
aquapaka <aqua.tamlong@gmail.com>
2023-12-06 03:04:36 +0700

    add new theme

---
+++ gruvbox-theme/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+Invoke-Expression (&starship init powershell)
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+$env:XDG_CONFIG_HOME = "$HOME/.config"
