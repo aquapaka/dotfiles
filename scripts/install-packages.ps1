87ad3e8e060f3bfaf5072100695a9613e9ca3517
aquapaka <aqua.tamlong@gmail.com>
2024-05-31 17:46:07 +0700

    Add tlinh

--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -1,17 +1,19 @@
-winget install Git -e;
+# Required
 winget install Alacritty -e;
 winget install GlazeWM -e;
+winget install --id MSYS2.MSYS2 -e;
+winget install --id eza-community.eza -e;
+winget install --id MikeFarah.yq
+
+# Application (comment out things you don't need before running this script)
+winget install Git -e -i;
 winget install --id Microsoft.PowerToys -e;
 winget install --id Discord.Discord -e;
 winget install --id Google.Chrome -e;
 winget install --id Google.GoogleDrive -e;
-winget install --id MSYS2.MSYS2 -e;
-winget install --id Valve.Steam -e;
 winget install "Lenovo Vantage";
 winget install --id 7zip.7zip -e;
-winget install --id VNGCorp.Zalo -e;
 winget install --id CoreyButler.NVMforWindows -e;
-winget install --id eza-community.eza -e;
-winget install AnyDesk -e;
-winget install -e --id Microsoft.VisualStudioCode -i --scope machine;
+winget install -e --id Microsoft.VisualStudioCode -i;
 winget install Notion.Notion;
+winget install --id pnpm.pnpm -e;
