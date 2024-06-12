ddc74c14657c4f873124c1fac63761a3c87b3b5f
aquapaka <aqua.tamlong@gmail.com>
2024-06-12 10:33:42 +0700

    Update

--- .dotfile/dot_config/wezterm/wezterm-theme.lua
+++ .dotfile/dot_config/wezterm/wezterm-theme.lua
@@ -4,13 +4,11 @@ local module = {}
 
 function module.apply_to_config(config)
   config.font = wezterm.font_with_fallback {
-    {
-      family = 'scientifica', weight = 'Medium'
-    },
-    { family = 'Pixelcraft Nerd Font', assume_emoji_presentation = true}
+    { family = 'Monaspace Radon', weight = 'Medium' },
   }
-  config.font_size = 16.5
-  config.cell_width = 1.1
+  config.font_size = 14
+  config.cell_width = 1
+  config.line_height = 1
   config.window_padding = {
     left = 16,
     right = 16,
@@ -21,54 +19,54 @@ function module.apply_to_config(config)
 
   config.colors = {
     -- The default text color
-    foreground = '#27fbfe',
+    foreground = '#d1ded3',
     -- The default background color
-    background = '#070219',
+    background = '#273136',
 
     -- Overrides the cell background color when the current cell is occupied by the
     -- cursor and the cursor style is set to Block
-    cursor_bg = '#F29AB2',
+    cursor_bg = '#7eb08a',
     -- Overrides the text color when the current cell is occupied by the cursor
-    cursor_fg = '#070219',
+    cursor_fg = '#d1ded3',
     -- Specifies the border color of the cursor when the cursor style is set to Block,
     -- or the color of the vertical or horizontal bar when the cursor style is set to
     -- Bar or Underline.
-    cursor_border = '#F29AB2',
+    cursor_border = '#7eb08a',
 
     -- the foreground color of selected text
-    selection_fg = '#27fbfe',
+    selection_fg = '#d2b48c',
     -- the background color of selected text
-    selection_bg = '#383E40',
+    selection_bg = '#484a46',
 
     ansi = {
-      "#626483",
-      "#fb007a",
-      "#a6e22e",
-      "#f3e430",
-      "#19bffe",
-      "#6800d2",
-      "#43fbff",
-      "#d9d9d9"
+      "#697f84",
+      "#ff819f",
+      "#7eb08a",
+      "#d2b48c",
+      "#7ea4b0",
+      "#ba8eaf",
+      "#82beb0",
+      "#d1ded3"
     },
     brights = {
-      "#626483",
-      "#fb007a",
-      "#a6e22e",
-      "#f3e430",
-      "#19bffe",
-      "#6800d2",
-      "#43fbff",
-      "#d9d9d9"
+      "#a5c8d0",
+      "#ff818e",
+      "#b4fcc5",
+      "#ffdaaa",
+      "#b4ebfc",
+      "#ffc2ef",
+      "#aeffec",
+      "#f0fff4"
     },
 
     -- Arbitrary colors of the palette in the range from 16 to 255
-    indexed = { [136] = "#fb007a" },
+    indexed = { [136] = "#7eb08a" },
 
     -- Since: 20220319-142410-0fcdea07
     -- When the IME, a dead key or a leader key are being processed and are effectively
     -- holding input pending the result of input composition, change the cursor
     -- to this color to give a visual cue about the compose state.
-    compose_cursor = "#f3e430"
+    compose_cursor = "#ffdaaa"
   }
 end
 
--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -1,13 +1,13 @@
-# Required
+# Required (Needed for everything to work)
 winget install WezTerm -e;
 winget install GlazeWM -e;
 winget install --id eza-community.eza -e;
 winget install --id MikeFarah.yq;
 winget install --i jqlang.jq;
 winget install --id MSYS2.MSYS2 -e;
+winget install Git -e -i;
 
 # Application (comment out things you don't need before running this script)
-winget install Git -e -i;
 winget install --id Microsoft.PowerToys -e;
 winget install --id Discord.Discord -e;
 winget install --id Google.Chrome -e;
@@ -15,6 +15,6 @@ winget install --id Google.GoogleDrive -e;
 winget install "Lenovo Vantage";
 winget install --id 7zip.7zip -e;
 winget install --id CoreyButler.NVMforWindows -e;
-winget install -e --id Microsoft.VisualStudioCode -i;
+winget install -e --id Microsoft.VisualStudioCode --scope machine -i;
 winget install Notion.Notion;
 winget install --id pnpm.pnpm -e;
