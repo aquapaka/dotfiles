4f795ddc7c3f36d98e556b11349311435517c185
aquapaka <aqua.tamlong@gmail.com>
2024-06-21 13:41:38 +0700

    feat: add khanhoa theme, allow switch light/dark mode between theme

--- .dotfile/dot_config/wezterm/wezterm-theme.lua
+++ .dotfile/dot_config/wezterm/wezterm-theme.lua
@@ -4,13 +4,11 @@ local module = {}
 
 function module.apply_to_config(config)
   config.font = wezterm.font_with_fallback {
-    {
-      family = 'scientifica', weight = 'Medium'
-    },
-    { family = 'Pixelcraft Nerd Font', assume_emoji_presentation = true}
+    { family = 'Monaspace Xenon', weight = "Regular"},
   }
-  config.font_size = 16.5
-  config.cell_width = 1.1
+  config.font_size = 13.5
+  config.cell_width = 1
+  config.line_height = 1
   config.window_padding = {
     left = 16,
     right = 16,
@@ -21,54 +19,54 @@ function module.apply_to_config(config)
 
   config.colors = {
     -- The default text color
-    foreground = '#27fbfe',
+    foreground = '#000000',
     -- The default background color
-    background = '#070219',
+    background = '#F2EEDE',
 
     -- Overrides the cell background color when the current cell is occupied by the
     -- cursor and the cursor style is set to Block
-    cursor_bg = '#F29AB2',
+    cursor_bg = '#000000',
     -- Overrides the text color when the current cell is occupied by the cursor
-    cursor_fg = '#070219',
+    cursor_fg = '#AAAAAA',
     -- Specifies the border color of the cursor when the cursor style is set to Block,
     -- or the color of the vertical or horizontal bar when the cursor style is set to
     -- Bar or Underline.
-    cursor_border = '#F29AB2',
+    cursor_border = '#CC3E28',
 
     -- the foreground color of selected text
-    selection_fg = '#27fbfe',
+    selection_fg = '#000000',
     -- the background color of selected text
-    selection_bg = '#383E40',
+    selection_bg = '#D8D5C7',
 
     ansi = {
-      "#626483",
-      "#fb007a",
-      "#a6e22e",
-      "#f3e430",
-      "#19bffe",
-      "#6800d2",
-      "#43fbff",
-      "#d9d9d9"
+      "#000000",
+      "#CC3E28",
+      "#216609",
+      "#B58900",
+      "#1E6FCC",
+      "#5C21A5",
+      "#158c86",
+      "#AAAAAA"
     },
     brights = {
-      "#626483",
-      "#fb007a",
-      "#a6e22e",
-      "#f3e430",
-      "#19bffe",
-      "#6800d2",
-      "#43fbff",
-      "#d9d9d9"
+      "#555555",
+      "#CC3E28",
+      "#216609",
+      "#B58900",
+      "#1E6FCC",
+      "#5C21A5",
+      "#158c86",
+      "#AAAAAA"
     },
 
     -- Arbitrary colors of the palette in the range from 16 to 255
-    indexed = { [136] = "#fb007a" },
+    indexed = { [136] = "#F2EEDE" },
 
     -- Since: 20220319-142410-0fcdea07
     -- When the IME, a dead key or a leader key are being processed and are effectively
     -- holding input pending the result of input composition, change the cursor
     -- to this color to give a visual cue about the compose state.
-    compose_cursor = "#f3e430"
+    compose_cursor = "#B58900"
   }
 end
 
--- .dotfile/dot_config/wezterm/wezterm.lua
+++ .dotfile/dot_config/wezterm/wezterm.lua
@@ -2,6 +2,10 @@
 local wezterm = require 'wezterm'
 local theme = require 'wezterm-theme'
 
+wezterm.on('format-window-title', function(tab, pane, tabs, panes, config)
+  return "Terminal"
+end)
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
