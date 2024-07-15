ac2817d58b21d03af227d6bb65fd864137b9b213
aquapaka <aqua.tamlong@gmail.com>
2024-07-15 16:56:46 +0700

    feat: package change

--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -6,16 +6,16 @@ winget install LGUG2Z.whkd --silent
 winget install --id eza-community.eza -e --silent;
 winget install --id MikeFarah.yq --silent;
 winget install --id jqlang.jq --silent;
-winget install --id Microsoft.PowerToys -e;
+winget install --id Microsoft.PowerToys -e --silent;
 winget install --id MSYS2.MSYS2 -e --silent;
 
 # Application (comment out things you don't need before running this script)
-winget install --id Discord.Discord -e;
-winget install --id Google.Chrome -e;
-winget install --id Google.GoogleDrive -e;
-winget install "Lenovo Vantage";
-winget install --id 7zip.7zip -e;
-winget install --id CoreyButler.NVMforWindows -e;
-# winget install -e --id Microsoft.VisualStudioCode -i;
-winget install Notion.Notion;
-winget install --id pnpm.pnpm -e;
+winget install --id Discord.Discord -e --silent;
+winget install --id Google.Chrome -e -i;
+winget install --id Google.GoogleDrive -e --silent;
+winget install "Lenovo Vantage" --silent;
+winget install --id 7zip.7zip -e --silent;
+winget install --id CoreyButler.NVMforWindows -e --silent;
+winget install -e --id Microsoft.VisualStudioCode -i;
+winget install Notion.Notion --silent;
+winget install --id pnpm.pnpm -e --silent;
