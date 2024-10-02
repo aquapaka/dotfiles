6c17d07615da0936047b129f7b63f9703ccfe5cb
aquapaka <aqua.tamlong@gmail.com>
2024-10-02 13:50:41 +0700

    fix: correct show cpu freq script key basename

---
+++ .changeset/seven-windows-add.md
@@ -0,0 +1,5 @@
+---
+"aquapaka-dotfiles": patch
+---
+
+fix: correct show cpu freq script key basename
--- scripts/show-cpu-frequency-power-plan-setting.ps1
+++ scripts/show-cpu-frequency-power-plan-setting.ps1
@@ -12,7 +12,7 @@ Showing maximum cpu frequency in power plan setting
 
 This will allow you to change maximum cpu frequency in power plan setting, which aiming to reduce temperature and improve stability
 #>
-$key = 'Computer\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100'
+$key = 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\54533251-82be-4824-96c1-47b60b740d00\75b0ae3f-bce0-45a7-8c89-c9611c25e100'
 $value = 'Attributes'
 $valueData = 2
 $valueType = [Microsoft.Win32.RegistryValueKind]::DWord
