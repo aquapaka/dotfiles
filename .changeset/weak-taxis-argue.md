11c1d5aad082048fd31f1d892e42769f09626a4c
aquapaka <aqua.tamlong@gmail.com>
2024-11-12 23:37:20 +0700

    chore: remove unused files

---
+++ .changeset/weak-taxis-argue.md
@@ -0,0 +1,5 @@
+---
+"aquapaka-dotfiles": patch
+---
+
+chore: remove unused files
--- scripts/terminal-cursor-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-
-# Related issue https://github.com/alacritty/alacritty/pull/6994
-cd "$parent_path"
-echo "Fixing terminal cursor..."
-cp ./conpty/conpty.dll 'C:\Program Files\Alacritty'
-cp ./conpty/OpenConsole.exe 'C:\Program Files\Alacritty'
-echo "Done! restart your terminal for change to take effect."
